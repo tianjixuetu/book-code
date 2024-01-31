fc74c2fdca27ff5e44692f6d2bc98b68f77b9b36
yunjinqi <yunjinqi@qq.com>
2024-01-31 10:09:50 +0800

    add httpclient and move java version from 8 to 21.0.1

---
+++ java/oanda-restapi/src/test/java/com/precioustech/fxtrading/oanda/restapi/account/OandaAccountDataProviderServiceTestTwo.java
@@ -0,0 +1,7 @@
+package com.precioustech.fxtrading.oanda.restapi.account;
+
+public class OandaAccountDataProviderServiceTestTwo {
+    public static void main(String[] args){
+        System.out.println("run test OandaAccountDataProviderServiceTestTwo");
+    }
+}
